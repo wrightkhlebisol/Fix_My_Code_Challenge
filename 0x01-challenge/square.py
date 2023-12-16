f891bfdd6c34ebdedb0a66184aacfd93930323dd
wrightkhlebisol <calebogundiya@gmail.com>
2023-12-16 22:07:54 +0000

    docstring

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
+"""Square class."""
+
 
 class square():
+    """Calculate square."""
 
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """Init perimeter."""
+
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """Area of the square."""
 
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """Perimeter of square."""
+
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """Print object."""
+
         return "{}/{}".format(self.width, self.height)
 
 
